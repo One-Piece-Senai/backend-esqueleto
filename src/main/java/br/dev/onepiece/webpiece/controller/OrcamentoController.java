5a0e4c87d848d01b63e288abd457c0403a9b947e
MarceloSilSantos <tecmarcelo78@outlook.com>
2024-11-25 22:08:01 -0300

    endpoint listar orcamento por status

--- src/main/java/br/dev/onepiece/webpiece/controller/OrcamentoController.java
+++ src/main/java/br/dev/onepiece/webpiece/controller/OrcamentoController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.dev.onepiece.webpiece.enums.StatusOrcamentos;
 import br.dev.onepiece.webpiece.model.Orcamento;
 import br.dev.onepiece.webpiece.model.Projeto;
 import br.dev.onepiece.webpiece.model.Usuario;
@@ -49,6 +50,22 @@ public class OrcamentoController {
        });
        return dtos;
     }
+    
+ // Listar orçamentos por status (escolher o Status)
+ // http://localhost:8080/orcamentos/listarPorStatus/APROVADO
+    @GetMapping("/listarPorStatus/{status}")
+    public ResponseEntity<List<OrcamentoRespostaDTO>> getOrcamentosByStatus(@PathVariable StatusOrcamentos status) {
+        // Buscar os orçamentos com o status especificado
+        List<Orcamento> orcamentos = orcamentoRepository.findByStatus(status); // Método personalizado no repositório
+        List<OrcamentoRespostaDTO> dtos = new ArrayList<>();
+        
+        orcamentos.forEach(o -> {
+            dtos.add(new OrcamentoRespostaDTO(o.getId(), o.getValor(), o.getDataEntrega(), o.getFormaPagamento(), o.getStatus(), o.getUsuario()));
+        });
+        
+        return ResponseEntity.ok(dtos); // Retorna a lista de orçamentos no formato DTO
+    }
+
     
     // Buscar orçamentos por ID do Projetista (idUsuario)
     @GetMapping("/listarPorUsuario/{idUsuario}")
--- src/main/java/br/dev/onepiece/webpiece/repository/OrcamentoRepository.java
+++ src/main/java/br/dev/onepiece/webpiece/repository/OrcamentoRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import br.dev.onepiece.webpiece.enums.StatusOrcamentos;
 import br.dev.onepiece.webpiece.model.Orcamento;
 
 public interface OrcamentoRepository extends JpaRepository<Orcamento, Long> {
@@ -12,4 +13,9 @@ public interface OrcamentoRepository extends JpaRepository<Orcamento, Long> {
     List<Orcamento> findByUsuarioId(Long idUsuario);
 
 	Orcamento findByUsuarioIdAndProjetoId(Long id, Long id2);
+	
+	 // Método para buscar orçamentos por status
+    List<Orcamento> findByStatus(StatusOrcamentos status);
+	
+	
 }
